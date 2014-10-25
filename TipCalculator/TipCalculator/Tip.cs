f916c3546f2c401511029011b0cdfe6aba98c1a1
kiide <kide.lemark@gmail.com>
2014-10-25 13:15:26 -0200

    finish app

---
+++ TipCalculator/TipCalculator/Tip.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipCalculator
+{
+    public class Tip
+    {
+        private readonly double _tipPercentage;
+
+        public decimal BillAmount { get; private set; }
+
+        public decimal TipAmount => BillAmount * (decimal)_tipPercentage;
+
+        public decimal TotalAmount => BillAmount + TipAmount;
+
+        public Tip(decimal billAmount, double tipPercentage)
+        {
+            BillAmount = billAmount;
+            _tipPercentage = tipPercentage;
+        }
+    }
+}
