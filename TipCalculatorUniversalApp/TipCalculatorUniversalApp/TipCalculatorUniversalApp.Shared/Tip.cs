6fdd06c84393bd9a61599766c7424b34446b8d32
kiide <kide.lemark@gmail.com>
2014-10-26 03:39:46 -0200

    finish universal app

---
+++ TipCalculatorUniversalApp/TipCalculatorUniversalApp/TipCalculatorUniversalApp.Shared/Tip.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TipCalculatorUniversalApp
+{
+    public class Tip
+    {
+        private readonly double _tipPercentage;
+
+        public decimal BillAmount { get; private set; }
+
+        public decimal TipAmount => BillAmount * (decimal)_tipPercentage;
+
+        public decimal TotalAmount => BillAmount + TipAmount;
+
+        public Tip(decimal billAmount, double tipPercentage)
+        {
+            BillAmount = billAmount;
+            _tipPercentage = tipPercentage;
+        }
+    }
+}
--- TipCalculatorUniversalApp/TipCalculatorUniversalApp/TipCalculatorUniversalApp.Windows/MainPage.xaml.cs
+++ TipCalculatorUniversalApp/TipCalculatorUniversalApp/TipCalculatorUniversalApp.Windows/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -22,9 +23,46 @@ namespace TipCalculatorUniversalApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private Tip _tip;
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void BillAmountTextBox_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            BillAmountTextBox.Text = _tip.BillAmount.ToString("C");
+        }
+
+        private void BillAmountTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            PeformCalculation();
+        }
+
+        private void BillAmountTextBox_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            BillAmountTextBox.Text = string.Empty;
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            PeformCalculation();
+        }
+
+        private void PeformCalculation()
+        {
+            decimal amount;
+            decimal.TryParse(BillAmountTextBox.Text, NumberStyles.Currency, CultureInfo.CurrentCulture, out amount);
+            var percent = Convert.ToDouble(StackPanel.Children.OfType<RadioButton>().First(x => x.IsChecked == true).Tag);
+            _tip = new Tip(amount, percent);
+            ShowResult();
+        }
+
+        private void ShowResult()
+        {
+            TipAmountTextBlock.Text = _tip.TipAmount.ToString("C", CultureInfo.CurrentCulture);
+            TotalAmountTextBlock.Text = _tip.TotalAmount.ToString("C", CultureInfo.CurrentCulture);
         }
     }
 }
