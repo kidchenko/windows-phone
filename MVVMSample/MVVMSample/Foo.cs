3a6a63d2ae7f001f633cf01716df8feb6488d2e9
kiide <kide.lemark@gmail.com>
2014-10-29 00:06:54 -0200

    create class Foo and implement INotifyPropertyChanged

---
+++ MVVMSample/MVVMSample/Foo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMSample
+{
+    public class Foo : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            // c# 6.0 see http://davefancher.com/2014/08/14/c-6-0-null-propagation-operator/
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
