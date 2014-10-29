3312a9f3be8c9ad5b4c2d4c06fc1c35ad30480c4
kiide <kide.lemark@gmail.com>
2014-10-29 03:03:37 -0200

    finish example about mvvm

--- MVVMSample/MVVMSample/MainPage.xaml.cs
+++ MVVMSample/MVVMSample/MainPage.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
+﻿using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
+// ReSharper disable RedundantExtendsListEntry
+
+// ReSharper disable ArrangeThisQualifier
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
 
@@ -27,6 +18,13 @@ namespace MVVMSample
             this.InitializeComponent();
 
             this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            var foo = new Foo
+            {
+                Id = 1,
+                Name = "Name"
+            };
+            this.DataContext = foo;
         }
 
         /// <summary>
