6fc41624e1c94763df3dbab79273d5c64183b9f7
kiide <kide.lemark@gmail.com>
2014-10-31 01:11:15 -0200

    add example about binding commands

---
+++ BindingToCommands/BindingToCommands/Car.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace BindingToCommands
+{
+    public class Car : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+        private string _checkInDateTime;
+
+        public string CheckedInDateTime
+        {
+            get { return _checkInDateTime; }
+            set
+            {
+                _checkInDateTime = value;
+                OnPropertyChanged();
+            } 
+        }
+
+        public ICommand CheckInCommand { get; set; } = new CheckInButtonClick();
+
+        public void CheckInCar()
+        {
+            CheckedInDateTime = DateTime.Now.ToString("t");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class CheckInButtonClick : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            ((Car)parameter).CheckInCar();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
---
+++ BindingToCommands/BindingToCommands/CarDataSource.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace BindingToCommands
+{
+    public class CarDataSource
+    {
+        private static readonly ObservableCollection<Car> Cars = new ObservableCollection<Car>();
+
+        public static ObservableCollection<Car> GetCars()
+        {
+            if (Cars.Any()) return Cars;
+            Cars.Add(new Car { Id = 1, Make = "Olds", Model = "Cutlas"});
+            Cars.Add(new Car { Id = 2, Make = "Geo", Model = "Prism" });
+            Cars.Add(new Car { Id = 3, Make = "Ford", Model = "Pinto" });
+            return Cars;
+        }
+    }
+}
