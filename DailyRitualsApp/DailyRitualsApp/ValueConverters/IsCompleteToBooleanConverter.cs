a2123cc1fa8ae919bd04f34ba70d0b08ef6b7221
kiide <kide.lemark@gmail.com>
2014-11-03 15:48:21 -0200

    add IsCompleteToBooleanConverter in folder ValueConverters

---
+++ DailyRitualsApp/DailyRitualsApp/ValueConverters/IsCompleteToBooleanConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace DailyRitualsApp.ValueConverters
+{
+    class IsCompleteToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var dates = (ObservableCollection<DateTime>) value;
+            return !dates.Contains(DateTime.Today);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
