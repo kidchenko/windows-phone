9d24d744e1b4939b6256ab6a03eeda91b13abad3
kiide <kide.lemark@gmail.com>
2014-11-03 15:54:53 -0200

    add CompletedDatesToIntegerConverter and finish example

---
+++ DailyRitualsApp/DailyRitualsApp/ValueConverters/CompletedDatesToIntegerConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace DailyRitualsApp.ValueConverters
+{
+    public class CompletedDatesToIntegerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var dates = (ObservableCollection<DateTime>) value;
+            return dates.Count;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
