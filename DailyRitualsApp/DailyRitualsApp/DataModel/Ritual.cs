7fd8f41921250c0312bfcd5d4ec55e9beca74d8a
kiide <kide.lemark@gmail.com>
2014-11-03 15:33:47 -0200

    implement INotifyProperyChanged

--- DailyRitualsApp/DailyRitualsApp/DataModel/Ritual.cs
+++ DailyRitualsApp/DailyRitualsApp/DataModel/Ritual.cs
@@ -1,17 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using DailyRitualsApp.Commands;
 
 namespace DailyRitualsApp.DataModel
 {
-    public class Ritual
+    public class Ritual : INotifyPropertyChanged
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public ObservableCollection<DateTime> Dates { get; set; }
+        public ObservableCollection<DateTime> Dates { get; set; } = new ObservableCollection<DateTime>();
+
+        [IgnoreDataMember]
+        public ICommand CompletedCommand { get; set; } = new CompletedButtonClick();
+
+        public void AddDate()
+        {
+            Dates.Add(DateTime.Today);
+            OnPropertyChanged("Dates");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
