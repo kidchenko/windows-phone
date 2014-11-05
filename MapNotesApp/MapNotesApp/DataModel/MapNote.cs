2d6528960b57047f77cd6e4c1fdb0064beac8f9a
kiide <kide.lemark@gmail.com>
2014-11-04 23:05:23 -0200

    add mapnote class

---
+++ MapNotesApp/MapNotesApp/DataModel/MapNote.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapNotesApp.DataModel
+{
+    public class MapNote
+    {
+        public string Title { get; set; }
+        public string Note { get; set; }
+        public DateTime Created { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+    }
+}
